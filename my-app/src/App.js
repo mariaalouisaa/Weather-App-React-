aef8e312468a763984baa66c060b100bce580fdf
= <maria.louisa.castro@gmail.com>
2021-11-25 09:26:38 -0600

    Created React File

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
+import sun from "./images/sun.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>WEATHER </p>
+        <img src={sun} className="App-logo" alt="logo" />
+        <p>WEATHER APP</p>
       </header>
     </div>
   );
