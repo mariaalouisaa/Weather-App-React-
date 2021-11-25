1860b73ff36e76a4a4012d85c4be4a2b3a8bfb30
= <maria.louisa.castro@gmail.com>
2021-11-25 14:45:55 -0600

    added target="_blank" to footer link

--- my-app/src/Footter.js
+++ my-app/src/Footter.js
@@ -2,7 +2,13 @@ export default function Footer() {
   return (
     <p>
       Coded by Maria Castro. Open Sources on{" "}
-      <a href="https://github.com/mariaalouisaa/Weather-App-React-">GitHub</a>.
+      <a
+        href="https://github.com/mariaalouisaa/Weather-App-React-"
+        target="_blank"
+      >
+        GitHub
+      </a>
+      .
     </p>
   );
 }
