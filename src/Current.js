7ea671ae1c89cacad0b2cd1302d7ceba6f59bf0b
= <maria.louisa.castro@gmail.com>
2021-11-26 09:51:23 -0600

    debugging

--- src/Current.js
+++ src/Current.js
@@ -4,6 +4,7 @@ import "./Current.css";
 export default function Current() {
   let cityInfo = {
     city: "London",
+    temp: 11,
     time: "12:00",
     date: "05/12/21",
   };
@@ -13,11 +14,7 @@ export default function Current() {
       <img src={sun} className="main-image rotate" alt="animated weather" />
       <div className="current-temp-items">
         <h1>{cityInfo.city}</h1>
-        <h2>
-          <span role="figure" aria-label="loading">
-            ⏳
-          </span>
-        </h2>
+        <h2>{cityInfo.temp}°C </h2>
         <div>
           <button type="button" className="celciusButton buttonClicked">
             °C
