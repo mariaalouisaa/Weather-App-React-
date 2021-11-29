ddb24ecae4939684f605f37e682faf79906345dd
= <maria.louisa.castro@gmail.com>
2021-11-28 18:42:21 -0600

    added netlify link

--- src/Footter.js
+++ src/Footter.js
@@ -1,13 +1,21 @@
 export default function Footer() {
   return (
     <p>
-      Coded by Maria Castro. Open Sources on{" "}
+      Coded by Maria Castro. Open Sourced on{" "}
       <a
         href="https://github.com/mariaalouisaa/Weather-App-React-"
         target="_blank"
         rel="noreferrer"
       >
-        GitHub
+        GitHub{" "}
+      </a>
+      and hosted on{" "}
+      <a
+        href="https://jolly-kirch-7434c0.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Netlify
       </a>
       .
     </p>
